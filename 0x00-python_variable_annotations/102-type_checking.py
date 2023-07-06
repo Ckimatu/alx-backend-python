0ff151e9be176b923dd49d2429ee700ca8d0dc6b
Caroline <carolinekimatu6@gmail.com>
2023-07-06 18:02:27 +0000

    mypy validation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,19 @@
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [
-        item for item in lst
-        for i in range(factor)
-    ]
+#!/usr/bin/env python3
+
+"""
+contains an annotated function
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Creates multiple copies of items in a tuple
+    """
+    zoomed_in: List = [
+            item for item in lst
+            for i in range(factor)
+            ]
     return zoomed_in
 
 
@@ -10,4 +21,4 @@ array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
