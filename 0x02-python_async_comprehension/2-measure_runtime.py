cd51f932cab0c2f76849fb75a2ce3f910875bda2
Caroline <carolinekimatu6@gmail.com>
2023-07-11 06:17:29 +0000

    Async Comprehension tasks

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+contains measure_runtime coroutine
+"""
+import asyncio
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    executes async_comprehension four times
+    in parallel using asyncio.gather
+    """
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = asyncio.get_event_loop().time()
+    total_runtime = end_time - start_time
+
+    return total_runtime
