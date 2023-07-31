cc51d4798e67d8f841ef79af6fdec59391b5c789
Caroline <carolinekimatu6@gmail.com>
2023-07-31 10:28:10 +0000

    updating assertion message

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,7 +31,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), "Key not found in nested map")
+        self.assertEqual(str(context.exception), path[-1])
 
 
 if __name__ == '__main__':
